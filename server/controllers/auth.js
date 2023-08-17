a3357fd4d5a95202529296ba4c6683b1c3fc908a
Craig Eaton <craigea92@gmail.com>
2023-08-17 15:03:41 +0100

    Added error handling message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
@@ -11,6 +11,6 @@ export const signup = async (req, res) => {
     await newUser.save()
     res.status(200).send("User has been created!")
   } catch (err) {
-    //todo
+    next(err)
   }
 };
