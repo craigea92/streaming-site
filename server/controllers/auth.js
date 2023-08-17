14cff177dd790807f40ad6c250ddfdac42495941
Craig Eaton <craigea92@gmail.com>
2023-08-17 14:33:28 +0100

    Added controllers and routes

---
+++ server/controllers/auth.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import User from "../models/User.js"
+
+export const signup = (req, res) => {
+  console.log(req.body)
+  // try { 
+  //   const newUser = new User(req.body)
+  // } catch(err) {
+  //   //todo
+  // }
+
+}
