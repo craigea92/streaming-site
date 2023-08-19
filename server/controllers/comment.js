f4bb9bcd79ff24c40893c51c7b915161995224a3
Craig Eaton <craigea92@gmail.com>
2023-08-19 09:37:51 +0100

    Added routes for comments

---
+++ server/controllers/comment.js
@@ -0,0 +1,23 @@
+export const addComment = async (req, res, next) => {
+  try {
+
+  } catch(err) {
+    next(err)
+  }
+}
+
+export const deleteComment = async (req, res, next) => {
+  try {
+
+  } catch(err) {
+    next(err)
+  }
+}
+
+export const getComments = async (req, res, next) => {
+  try {
+
+  } catch(err) {
+    next(err)
+  }
+}
