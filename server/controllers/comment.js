dcbde6caf8ea7b4f89276a1690e8c0fe594d9364
Craig Eaton <craigea92@gmail.com>
2023-08-20 11:22:33 +0100

    Added deletecomment function, users and video owners can delete comments

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,23 +1,35 @@
-export const addComment = async (req, res, next) => {
-  try {
+import { createError } from "../error";
+import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
 
-  } catch(err) {
-    next(err)
+export const addComment = async (req, res, next) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id });
+  try {
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment);
+  } catch (err) {
+    next(err);
   }
-}
+};
 
 export const deleteComment = async (req, res, next) => {
   try {
-
-  } catch(err) {
-    next(err)
+    const comment = await Comment.findById(res.params.id);
+    const video = await Video.findById(res.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment has been deleted");
+    } else {
+      return next(createError(403, "You can delete only your comment!"));
+    }
+  } catch (err) {
+    next(err);
   }
-}
+};
 
 export const getComments = async (req, res, next) => {
   try {
-
-  } catch(err) {
-    next(err)
+  } catch (err) {
+    next(err);
   }
-}
+};
