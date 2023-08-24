fa4a3341c31a331fa1ed9b8335a125199e164353
Craig Eaton <craigea92@gmail.com>
2023-08-24 09:21:11 +0100

    Added fetchvideo to map the data to cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
+import axios from "axios";
 
 const Container = styled.div`
   display: flex;
@@ -9,28 +10,20 @@ const Container = styled.div`
 `;
 
 const Home = () => {
+  const [videos, SetVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("/videos/random");
+      SetVideos(res.data);
+    };
+    fetchVideos();
+  }, []);
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map((video) => (
+        <Card />
+      ))}
     </Container>
   );
 };
