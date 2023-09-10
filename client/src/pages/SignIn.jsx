373ba0cb02150864ad14a27d2eef6e23d64f9cea
Craig Eaton <craigea92@gmail.com>
2023-09-10 12:12:14 +0100

    Updated classNames for SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,68 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: calc(100vh - 56px);
-  color: ${({ theme }) => theme.text};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.bgLighter};
-  border: 1px solid ${({ theme }) => theme.soft};
-  padding: 20px 50px;
-  gap: 10px;
-`;
-
-const Title = styled.h1`
-  font-size: 24px;
-`;
-
-const SubTitle = styled.h2`
-  font-size: 20px;
-  font-weight: 300;
-`;
-
-const Input = styled.input`
-  border: 1px solid ${({ theme }) => theme.soft};
-  border-radius: 3px;
-  padding: 10px;
-  background-color: transparent;
-  width: 100%;
-  color: ${({ theme }) => theme.text};
-`;
-
-const Button = styled.button`
-  border-radius: 3px;
-  border: none;
-  padding: 10px 20px;
-  font-weight: 500;
-  cursor: pointer;
-  background-color: ${({ theme }) => theme.soft};
-  color: ${({ theme }) => theme.textSoft};
-`;
-
-const More = styled.div`
-  display: flex;
-  margin-top: 10px;
-  font-size: 12px;
-  color: ${({ theme }) => theme.textSoft};
-`;
-
-const Links = styled.div`
-  margin-left: 50px;
-`;
-
-const Link = styled.span`
-  margin-left: 30px;
-`;
 
 const SignIn = () => {
   const [name, setName] = useState("");
@@ -76,38 +13,38 @@ const SignIn = () => {
     } catch (err) {}
   };
   return (
-    <Container>
-      <Wrapper>
-        <Title>Sign in</Title>
-        <SubTitle>to continue to LamaTube</SubTitle>
-        <Input
+    <div className="Container">
+      <div className="Wrapper">
+        <div className="Title">Sign in</div>
+        <div className="SubTitle">to continue to CraigTube</div>
+        <div className="Input"
           placeholder="username"
           onChange={(e) => setName(e.target.value)}
         />
-        <Input type="password" placeholder="password" />
-        <Button onClick={handleLogin}>Sign in</Button>
-        <Title>or</Title>
-        <Input
+        <div className="Input" type="password" placeholder="password" />
+        <div className="Button" onClick={handleLogin}>Sign in</div>
+        <div className="Title">or</div>
+        <div className="Input"
           placeholder="username"
           onChange={(e) => setName(e.target.value)}
         />
-        <Input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
-        <Input
+        <div className="Input" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <div className="Input"
           type="password"
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button>Sign up</Button>
-      </Wrapper>
-      <More>
+        <div className="Button">Sign up</div>
+      </div>
+      <div className="More">
         English(USA)
-        <Links>
-          <Link>Help</Link>
-          <Link>Privacy</Link>
-          <Link>Terms</Link>
-        </Links>
-      </More>
-    </Container>
+        <div className="Links">
+          <div className="Link">Help</div>
+          <div className="Link">Privacy</div>
+          <div className="Link">Terms</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
