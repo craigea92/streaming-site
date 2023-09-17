5c1ff0c924b06a2ec1cf01c5565b093f52c1ce7c
Craig Eaton <craigea92@gmail.com>
2023-09-17 15:54:14 +0100

    Imported Comments CSS and changed the class names

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,46 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import "./Comments.css";
 import Comment from "./Comment";
-
-const Container = styled.div``;
-
-const NewComment = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`;
-
-const Avatar = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-`;
-
-const Input = styled.input`
-  border: none;
-  border-bottom: 1px solid ${({ theme }) => theme.soft};
-  color: ${({ theme }) => theme.text};
-  background-color: transparent;
-  outline: none;
-  padding: 5px;
-  width: 100%;
-`;
 
 const Comments = () => {
   return (
-    <Container>
-      <NewComment>
-        <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
-        <Input placeholder="Add a comment..." />
-      </NewComment>
-      <Comment/>
-      <Comment/>
-      <Comment/>
-      <Comment/>
-      <Comment/>
-      <Comment/>
-      <Comment/>
-    </Container>
+    <div className="Comments_Container">
+      <div className="Comments_NewComment">
+        <div
+          className="Comments_Avatar"
+          src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <div className="Input" placeholder="Add a comment..." />
+      </div>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+    </div>
   );
 };
 
